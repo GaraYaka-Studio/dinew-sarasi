cc8d245c9f78be9c104383cd28b8e8cc336c86b3
Leven Rochana <levenrok@proton.me>
2026-02-10 16:05:45 +0530

    setup drizzle for the main postgres database

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+if (!process.env.DATABASE_URL) throw new Error("Main Database URL is not set! (DATABASE_URL)");
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+import * as schema from './schema';
+
+if (!process.env.DATABASE_URL) throw new Error("Database URL is not set! (DATABASE_URL)");
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle({
+    client: pool,
+    schema,
+});
