46fb927fee1835b36b1baa44da5bff0cef0e0914
Leven Rochana <levenrok@proton.me>
2026-02-10 17:06:18 +0530

    rename database tables

--- drizzle/0000_create_students_table.sql
+++ drizzle/0000_create_students_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "student" (
+CREATE TABLE "students" (
 	"id" integer PRIMARY KEY NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"grade" integer NOT NULL,
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
-import { date, integer, pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
+import { date, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const students = pgTable("student", {
+export const students = pgTable("students", {
     id: integer().primaryKey(),
     name: varchar({ length: 255 }).notNull(),
     grade: integer().notNull(),
